1ab319a1101c724a4f4b40432c0ea2c58321a1a2
Denys Kovalchuk <denkov414@gmail.com>
2024-10-19 19:45:20 +0300

    make build

---
+++ index.8ed44974.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");t.style.display="flex",t.style.justifyContent="center",t.style.alignItems="center",e.style.position="relative";
+//# sourceMappingURL=index.8ed44974.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+wall.style.display = "flex";
+wall.style.justifyContent = "center";
+wall.style.alignItems = "center";
+spider.style.position = "relative";
+
+//# sourceMappingURL=index.f75de5e1.js.map
